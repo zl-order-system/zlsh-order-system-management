3273fd7a1f51c2ec1d95ef90d77e6c3a2d59c8d2
tucker <tucker961220@gmail.com>
2023-12-16 20:50:41 +0800

    create project and edit index page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Index from './pages';
+
+function App() {
+  return (
+    <Router>
+        <Routes>
+          <Route path="/" element={<Index/>} />
+        </Routes>
+    </Router>
+  )
+}
+
+export default App
