586ddc76b4c3012af1f962ec181e8e2f4b0ce1b1
october <octoberserver@gmail.com>
2024-02-16 23:20:41 +0800

    Fix file capitalisation issue

--- src/util/AppConstants.ts
+++
@@ -1,6 +0,0 @@
-const appConstants = {
-    backendHost: process.env.BACKEND_HOST || "https://staging.order-system.octoberserver.net",
-    mainAppURL: process.env.MAIN_APP_URL || "https://zl-order-system.github.io/staging/app/",
-}
-export default appConstants;
-
